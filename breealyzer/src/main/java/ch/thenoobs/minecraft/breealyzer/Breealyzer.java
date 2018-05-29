f5311f32dc94438ede66fc84f705613341af059f
test <test@localhost>
2018-05-29 10:46:10 +0200

    test

--- breealyzer/src/main/java/ch/thenoobs/minecraft/breealyzer/Breealyzer.java
+++ breealyzer/src/main/java/ch/thenoobs/minecraft/breealyzer/Breealyzer.java
@@ -22,7 +22,6 @@ public class Breealyzer
     public static final String VERSION = "0.1";
         
     
-    
     @SidedProxy(serverSide = "ch.thenoobs.minecraft.breealyzer.proxies.CommonProxy", clientSide = "ch.thenoobs.minecraft.breealyzer.proxies.ClientProxy")
     public static CommonProxy proxy;
     
