9685b91e8dfe45c514327f4d69f04231f6945316
eti <eti@localhost>
2018-05-29 10:40:34 +0200

    test

--- breealyzer/src/main/java/ch/thenoobs/minecraft/breealyzer/Breealyzer.java
+++ breealyzer/src/main/java/ch/thenoobs/minecraft/breealyzer/Breealyzer.java
@@ -20,8 +20,7 @@ public class Breealyzer
     public static final String MOD_ID = "breealyzer";
     public static final String NAME = "Breealyzer";
     public static final String VERSION = "0.1";
-    
-    
+        
     
     @SidedProxy(serverSide = "ch.thenoobs.minecraft.breealyzer.proxies.CommonProxy", clientSide = "ch.thenoobs.minecraft.breealyzer.proxies.ClientProxy")
     public static CommonProxy proxy;
